e794fd75d5d1106266c1fcf70b5a47eb55bb16f8
alfrida22 <alfridaazzara@gmail.com>
2023-09-27 14:36:03 +0700

    fitur insert dan read

---
+++ app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('insertproducts', 'ProductController::insertProduct');
+$routes->post('insertproducts', 'ProductController::insertProduct');
+$routes->get('products', 'ProductController::readProduct');
