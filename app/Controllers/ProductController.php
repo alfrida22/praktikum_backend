c6f4171399ebefaa56d3d5311d15b5396bc35e61
alfrida22 <alfridaazzara@gmail.com>
2023-10-26 11:36:22 +0700

    Api

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
+use CodeIgniter\API\ResponseTrait;
 
 class ProductController extends BaseController {
+    use ResponseTrait;
 
     private $product;
 
@@ -42,6 +44,18 @@ class ProductController extends BaseController {
         return view('product', $data);
     }
 
+    public function readProductApi(){
+        $products = $this->product->findAll();
+
+        return $this->respond(
+            [
+                'code' => 200,
+                'status' => "OK",
+                'data' => $products
+            ]
+        );
+    }
+
     public function getProduct($id){
         $product = $this->product->where('id', $id)->first();
         $data = [
@@ -49,6 +63,31 @@ class ProductController extends BaseController {
         ];
         return view('edit_product', $data);
     }
+
+    
+
+    public function getProductApi($id){
+        $product = $this->product->where('id', $id)->first();
+       
+        if (!$product){
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON(
+                [
+                    'code'=> 404,
+                    'status'=> 'NOT FOUND',
+                    'data'=> 'product not found'
+                ]
+            );
+        }
+    
+        return $this->respond([
+            'code' => 200,
+            'status' => "OK",
+            'data' => $product
+    ]
+    );
+}
+    
     public function updateProduct($id){
         $nama_product = $this->request->getVar('nama_product');
         $description = $this->request->getVar('description');
